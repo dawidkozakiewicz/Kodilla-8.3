c15b435ffa3f79256df8c9c7d38b602db81e9e1b
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-01-25 22:12:49 +0100

    skasowanie omylkowo wprowadzonych zmian

--- js/sayhello.js
+++ js/sayhello.js
@@ -1,9 +1,3 @@
 var name = prompt('Wprowadz swoje imie');
-// alert przestal sie wyswietlac alert('Siemka, ' + name + '!');
+alert('Siemka, ' + name + '!');
 console.log('Siemka, ' + name +'!');
-// to jest komentarz liniowy
-
-/* to
-jest
-komentarz
-blokowy */
